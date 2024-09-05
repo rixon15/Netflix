6c589ed8171eddcf205163630d76e50c461d3887
unknown <ivacsony.szilard@gmail.com>
2024-09-05 08:22:17 -0700

    tv + movie controlleres, routes created

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 
 import authRoutes from "./routes/auth.route.js";
+import movieRoutes from "./routes/movie.route.js";
+import tvRoutes from "./routes/tv.route.js"
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
 
@@ -15,6 +17,8 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", movieRoutes);
+app.use("/api/v1/tv", tvRoutes);
 
 app.listen(PORT, () => {
   console.log("Server started at http://localhost:5000");
