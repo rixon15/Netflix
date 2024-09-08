6d9422585f5facf37733c3cfdb953b7fc890cc83
unknown <ivacsony.szilard@gmail.com>
2024-09-08 01:16:05 -0700

    Created the search routes and controller

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -24,6 +24,7 @@ export const protectRoute = async(req, res, next) => {
 
         req.user = user;
 
+
         next()
     } catch (error) {
         console.log('Error in protectRoute middleware: ', error.message);
--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
 import cookieParser from "cookie-parser";
 import { protectRoute } from "./middleware/protectRoute.js";
+import searchRoute from './routes/search.route.js'
 
 const app = express();
 
@@ -22,6 +23,7 @@ app.get("/", (req, res) => {
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
+app.use('/api/v1/search', protectRoute, searchRoute);
 
 app.listen(PORT, () => {
   console.log("Server started at http://localhost:5000");
