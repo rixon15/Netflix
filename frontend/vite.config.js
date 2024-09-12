1f9f27c43818a91c7ef5e25733564f8bf25d407c
unknown <ivacsony.szilard@gmail.com>
2024-09-12 10:40:20 -0700

    final product

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://netflix-clone-indol-two-70.vercel.app",
+        target: "http://localhost:5000",
       },
     },
   },
