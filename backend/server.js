058af477af367563c7de113f5bf65dff4e9674e4
unknown <ivacsony.szilard@gmail.com>
2024-09-12 10:32:56 -0700

    final product

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
 import express from "express";
 import path from "path";
 
-import authRoutes from "../backend/routes/auth.route.js";
-import movieRoutes from "../backend/routes/movie.route.js";
-import tvRoutes from "../backend/routes/tv.route.js";
-import { ENV_VARS } from "../backend/config/envVars.js";
-import { connectDB } from "../backend/config/db.js";
+import authRoutes from "./routes/auth.route.js";
+import movieRoutes from "./routes/movie.route.js";
+import tvRoutes from "./routes/tv.route.js";
+import { ENV_VARS } from "./config/envVars.js";
+import { connectDB } from "./config/db.js";
 import cookieParser from "cookie-parser";
-import { protectRoute } from "../backend/middleware/protectRoute.js";
-import searchRoute from "../backend/routes/search.route.js";
+import { protectRoute } from "./middleware/protectRoute.js";
+import searchRoute from "./routes/search.route.js";
 
 const app = express();
 
